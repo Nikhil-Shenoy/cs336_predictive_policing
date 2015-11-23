3b4a83fe67b35f1e6d5ee861abcf9293ec1cf27e
Nikhil-Shenoy <nikhil_shenoy10@yahoo.com>
2015-11-22 22:27:15 -0500

    tested out connection to database

--- policing/index.js
+++ policing/index.js
@@ -2,6 +2,19 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var path = __dirname + '/static/';
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'policing.cke0kirlh7a4.us-east-1.rds.amazonaws.com',
+  user     : 'nikhil',
+  password : 'cs336shenoy',
+  database : 'BarBeerDrinker'
+});
+
+connection.connect();
+
+// app.set('view engine','jade');
+
+query = "SELECT * from sells";
 
 
 router.use(function(req, res,next) {
@@ -11,6 +24,13 @@ router.use(function(req, res,next) {
 
 router.get("/",function(req,res){
 	res.sendFile(path + "index.html");
+	connection.query(query, function(err,rows,fields) {
+		// if(err) throw err;
+		// console.log('The solution is: ',rows)
+		res.render("sells",{items : rows});
+		// res.send(rows);
+	});
+
 });
 
 router.get("/about",function(req,res) {
